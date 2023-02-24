74b880d8bb7fe73bdb01b83dd6cbb9ba95020623
ivynya <ivy@ivy.direct>
2023-02-23 21:32:52 -0800

    repo: update default schema

--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -3,19 +3,26 @@
 	export let array: any[];
 	$: json = JSON.stringify(array);
 
-	$: schemaString =
-		'{"id": "string","name": "string","description": "string","longDescription": "string","startDate": "string","endDate": "string","tags": "array","type": "array"}';
-	const onSchemaChange = () => {
-		$example = JSON.parse(schemaString);
-	};
+	$: properties = Object.keys($example).map((key) => {
+		return { name: key, type: $example[key] };
+	});
 </script>
 
 <section>
 	<h2>JSON Output</h2>
 	<textarea bind:value={json} />
 	<br /><br />
-	<h3>Modify Schema</h3>
-	<textarea class="in" on:change={onSchemaChange} bind:value={schemaString} />
+	<h2>Modify Schema</h2>
+	{#each properties as prop}
+	<div class="property">
+		<input type="text" name="sc-name-1" id="sc-name-1" placeholder="Property Name" value={prop.name}>
+		<select name="sc-type-1" id="sc-type-1" value={prop.type}>
+			<option value="string">String</option>
+			<option value="boolean">Bool</option>
+			<option value="array">Array</option>
+		</select>
+	</div>
+	{/each}
 </section>
 
 <style>
@@ -27,5 +34,24 @@
 	}
 	.in {
 		min-height: 150px;
+	}
+
+	.property {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		column-gap: 5px;
+
+		margin-bottom: 8px;
+	}
+
+	.property input {
+		flex: 1 1;
+	}
+
+	.property input, .property select {
+		font-size: 0.7rem;
+		padding: 5px 10px;
 	}
 </style>
--- src/lib/Skiff/schema.ts
+++ src/lib/Skiff/schema.ts
@@ -1,12 +1,28 @@
 import { writable } from "svelte/store";
 
 export let example = writable({
-  id: 'string',
-  name: 'string',
-  description: 'string',
-  longDescription: 'string',
-  startDate: 'string',
-  endDate: 'string',
-  tags: 'array',
-  type: 'array'
+  "id": "string",
+  "name": "string",
+  "organization": "string",
+
+  "description": "string",
+  "descriptionLong": "string",
+  "startDate": "string",
+  "endDate": "string",
+  "tags": "array",
+
+  "license": "array",
+
+  /*"plugins": {
+    "embed"?: string,
+    "github"?: string,
+    "link"?: string,
+    "merch"?: string,
+  },*/
+
+  "pastPositions": "array",
+
+  "display": "string",
+  "old": "boolean",
+  "type": "array"
 });
